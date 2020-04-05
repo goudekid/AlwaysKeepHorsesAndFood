026c312c530648e554a05e1201a23db5d7e63f41
Garrett Cox <goudekid@hotmail.com>
2020-04-05 13:42:23 -0700

    Add project files.

---
+++ AlwaysKeepHorsesAndFood/Patches.cs
@@ -0,0 +1,35 @@
+﻿using TaleWorlds.InputSystem;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Craft.WeaponDesign;
+using System;
+using TaleWorlds.Core;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Craft;
+using TaleWorlds.CampaignSystem.ViewModelCollection.GameMenu;
+
+namespace AlwaysKeepHorsesAndFood
+{
+    [HarmonyPatch(typeof(PartyVM))]
+    [HarmonyPatch(new Type[] { typeof(Game), typeof(PartyScreenLogic), typeof(string) })]
+    [HarmonyPatch(MethodType.Constructor)]
+    class GetPartyVMPostfix
+    {
+        public static void Postfix(PartyVM __instance)
+        {
+            
+        }
+
+    }
+
+    [HarmonyPatch(typeof(PartyVM), "ExecuteDone")]
+    class RemoveOldPartyVMPostfix
+    {
+        public static void Postfix()
+        {
+        }
+    }
+
+
+}
---
+++ AlwaysKeepHorsesAndFood/SubModule.cs
@@ -0,0 +1,30 @@
+﻿using TaleWorlds.MountAndBlade;
+using HarmonyLib;
+using TaleWorlds.InputSystem;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Craft.WeaponDesign;
+using System.IO;
+using System;
+using TaleWorlds.Core;
+using TaleWorlds.CampaignSystem.ViewModelCollection.GameMenu;
+using System.Linq;
+
+namespace AlwaysKeepHorsesAndFood
+{
+    class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            Harmony harmony = new Harmony("com.goog.modname");
+            harmony.PatchAll();
+            base.OnSubModuleLoad();
+        }
+
+        private void SendMessage(string s)
+        {
+            InformationManager.DisplayMessage(new InformationMessage(s));
+        }
+    }
+
+}
